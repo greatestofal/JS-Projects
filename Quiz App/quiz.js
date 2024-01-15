8b9a73f3300be5ab4d4ecdd84e35baba4e6fa0f3
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-01-15 15:03:46 +0100

    Corrected some errors

--- Quiz App/quiz.js
+++ Quiz App/quiz.js
@@ -33,39 +33,61 @@ const quizData = [
   },
 ];
 
-const quiz = document.getElementById("quiz");
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById("submit");
+const quiz = document.getElementById("quiz")
+const answerEls = document.querySelectorAll(".answer")
+const questionEl = document.getElementById("question")
 
-let currentQuiz = 0;
-let score = 0;
+const a_text = document.getElementById("a_text")
+const b_text = document.getElementById("b_text")
+const c_text = document.getElementById("c_text")
+const d_text = document.getElementById("d_text")
+const submitBtn = document.getElementById("submit")
 
-loadQuiz();
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
 function loadQuiz() {
-  deselectAnswers();
-  const currentQuizData = quizData[currentQuiz];
-  questionEl.innerText = currentQuizData.question;
-  a_text.innerText = currentQuizData.a;
-  b_text.innerText = currentQuizData.b;
-  c_text.innerText = currentQuizData.c;
-  d_text.innerText = currentQuizData.d;
+  deselectAnswers()
+  const currentQuizData = quizData[currentQuiz]
+  questionEl.innerText = currentQuizData.question
+  a_text.innerText = currentQuizData.a
+  b_text.innerText = currentQuizData.b
+  c_text.innerText = currentQuizData.c
+  d_text.innerText = currentQuizData.d
 }
 
 function deselectAnswers() {
-  answerEls.forEach((answerEls) => (answerEls.checked = false));
+  answerEls.forEach((answerEls) => (answerEls.checked = false))
 }
 
 function getSelected() {
-  let answerEls;
+  let answerEls
   answerEls.forEach((answerEls) => /*check for errors */ {
     if (answerEls.checked) {
-      answer = answerEls.id;
+      answer = answerEls.id
     }
-  });
-  return answer;
+  })
+  return answer
 }
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected()
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++
+    }
+    currentQuiz++
+    if ((current < quizData, length)) {
+      loadQuiz()
+    } else {
+      quiz.innerHTML = `
+        <h2>
+          You answer ${score}/ ${quizData.length} questions correctly
+        </h2>
+
+        <button onclick='location.reload()'> Reload </button>
+        `
+    }
+  }
+});
