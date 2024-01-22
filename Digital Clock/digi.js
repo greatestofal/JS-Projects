28aada3d6466112c568c244ca1c824697d506016
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-01-22 14:39:44 +0100

    Added Milliseconds to it.

--- Digital Clock/digi.js
+++ Digital Clock/digi.js
@@ -1,6 +1,7 @@
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('min');
 const seconds = document.getElementById('sec');
+const milli = document.getElementById('milli')
 
 setInterval(()=>{
 
@@ -9,6 +10,7 @@ setInterval(()=>{
     hours.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
     min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
     sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+    milli.innerHTML = (currentTime.getMilliseconds()<10?'0':'') + currentTime.getMilliseconds();
 
 },1000)
 
